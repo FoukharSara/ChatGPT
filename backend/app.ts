58558b4daa8a21417c6415d22475ce45e70bb505
FOUKHAR Iliass <if@bility.fr>
2024-06-27 11:25:16 +0200

    :recycle: Some refacto & :whale: Docker network

--- backend/app.ts
+++ backend/app.ts
@@ -1,63 +1,50 @@
 import fastify from "fastify";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import index from './src/routes/index';
-
+import { indexHandler } from "./src/routes";
 
 dotenv.config();
 
 const app = fastify({
-	logger: true,
+  logger: true,
 });
 
-const { DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
-
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, API_PORT, NODE_ENV } =
+  process.env;
 
 export async function connect(): Promise<any> {
-	mongoose.connect(
-		`mongodb://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}`,
-		{
-			socketTimeoutMS: 480000,
-			
-		}
-	);
+  mongoose.connect(
+    `mongodb://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}`,
+    {
+      socketTimeoutMS: 480000,
+    }
+  );
 }
 connect()
-	.then(() => {
-		console.log(`Database running at port 27017`);
-	})
-	.catch((err) => {
-		console.log("Failed to connect to database");
-		console.error(err);
-		process.exit(1);
-	});
+  .then(() => {
+    console.log(`Database running at port 27017`);
+  })
+  .catch((err) => {
+    console.log("Failed to connect to database");
+    console.error(err);
+    process.exit(1);
+  });
 
 //route
-app.register(index);
-
-
-
-const { API_PORT, NODE_ENV } = process.env;
+app.get("/", indexHandler);
 
 const options = {
-	port: parseInt(API_PORT ?? "3000"),
-	host: "0.0.0.0",
+  port: parseInt(API_PORT ?? "3000"),
+  host: "0.0.0.0",
 };
 
-// fetch('https://www.youtube.com/')
-//   .then(data => console.log('Data:', data))
-//   .catch(error => console.error('Error fetching data:', error));
-
-
 app.listen(options, (err, address) => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	}
-	console.log(`Worker ${process.pid} started`);
-	console.log(`Api running at ${address}`);
-	console.log(`Started at ${new Date()}`);
-	console.log(`Environment => ${NODE_ENV}`);
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Worker ${process.pid} started`);
+  console.log(`Api running at ${address}`);
+  console.log(`Started at ${new Date()}`);
+  console.log(`Environment => ${NODE_ENV}`);
 });
-
-
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,32 +1,54 @@
-import { FastifyInstance } from 'fastify';
-import resPrompt from '../models/response'; 
+import PromptResponse from "../models/response";
+import { FastifyRequest, FastifyReply } from "fastify";
 
-// export async function saveData() {
-    //     try {
-    //         const newData = {
-    //             prompt: 'What is your favorite color?',
-    //             response: 'My favorite color is blue.'
-    //         };
-    //         const result = await resPrompt.create(newData);
-    //         console.log('Data saved successfully:', result);
-    //     } catch (error) {
-    //         console.error('Error saving data:', error);
-    //     }
-    // }
-    
-    // saveData();
-export default async function indexRoute(fastify: FastifyInstance) {
-     fastify.get('/', async (req, res) => {
-        console.log(await resPrompt.find());
-        try {
-            const prompt = await resPrompt.find();
-            if (prompt) {
-                res.send(prompt);
-            } else {
-                res.status(404).send({ message: 'No prompt found' });
-            }
-        } catch (error) {
-            res.status(500).send({ error: 'Error fetching prompt from database' });
-        }
-    });
+export async function saveData() {
+  try {
+    const newData = {
+      prompt: "What is your favorite color?",
+      response: "My favorite color is blue.",
+    };
+    const result = await PromptResponse.create(newData);
+    console.log("Data saved successfully:", result);
+  } catch (error) {
+    console.error("Error saving data:", error);
+  }
 }
+
+// saveData();
+// export default async function indexRoute(fastify: FastifyInstance) {
+//      fastify.get('/', async (req, res) => {
+//         console.log(await resPrompt.find());
+//         try {
+//             const prompt = await resPrompt.find();
+//             if (prompt) {
+//                 res.send(prompt);
+//             } else {
+//                 res.status(404).send({ message: 'No prompt found' });
+//             }
+//         } catch (error) {
+//             res.status(500).send({ error: 'Error fetching prompt from database' });
+//         }
+//     });
+// }
+
+// REVIEW Sample route handler
+export const sampleHandler = (req: FastifyRequest, res: FastifyReply) => {
+  return res.send({ hello: "world" });
+};
+
+export const indexHandler = async (req: FastifyRequest, res: FastifyReply) => {
+  console.log("Hna");
+  await saveData();
+  console.log(await PromptResponse.find({}));
+  console.log("hnayaa");
+  try {
+    const prompt = await PromptResponse.find({});
+    if (prompt) {
+      res.send(prompt);
+    } else {
+      res.status(404).send({ message: "No prompt found" });
+    }
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching prompt from database" });
+  }
+};
