47f685479dd29b151176f4ed07aba21b0523325c
sara <foukharsara@gmail.com>
2024-07-01 14:55:00 +0100

    example harcoded

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
+let result : string= "";
 
 // config the api 
 const openai = new OpenAI({
@@ -17,33 +17,34 @@ interface PromptRequestBody {
   prompt: string;
 }
 
-export const promptHandler = (req:FastifyRequest, res:FastifyReply)=>{
+export const promptHandler = async (req:FastifyRequest, res:FastifyReply)=>{
   const { prompt  } = req.body as PromptRequestBody;
   try{
+    const response = {
+      choices: [{ text: "My current result." }],
+  }
+    result = response.choices[0].text;
+    saveData(prompt,result);
     return res.status(200).send({
       prompt,
+      response:response.choices[0].text,
     });
   }catch(error){
-    console.log(error);
+    res.status(500).send(error);
+    res.status(400).send(error);
   }
 }
 
 
-
-
-
-
-
-
-// export async function saveData() {
-//   try {
-//     const newData = {
-//       prompt: "What is your favorite color?",
-//       response: "My favorite color is blue.",
-//     };
-//     const result = await PromptResponse.create(newData);
-//     console.log("Data saved successfully:", result);
-//   } catch (error) {
-//     console.error("Error saving data:", error);
-//   }
-// }
+export async function saveData(prompt:string, response:string) {
+  try {
+    const newData = {
+      prompt: prompt,
+      response: result,
+    };
+    const results = await PromptResponse.create(newData);
+    console.log("Data saved successfully:", results);
+  } catch (error) {
+    console.error("Error saving data:", error);
+  }
+}
